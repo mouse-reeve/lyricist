ae1320ddc3961aa97061745491abe6b595bee7c8
Raj Shekhar <rajlist@rajshekhar.net>
2017-11-20 00:29:25 -0800

    fix for https://github.com/mouse-reeve/lyricist/issues/3 to handle spaces in file names

--- sing.sh
+++ sing.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # hope and pray the input is a pdf filepath
-pdf=$1
+pdf="$1"
 
-convert $pdf -append image-version.png
+convert "$pdf" -append image-version.png
 tesseract image-version.png text-version -psm 11
 
 say -o audio-version.aiff < text-version.txt
